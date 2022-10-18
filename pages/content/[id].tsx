3c2c4d29c11445b8646ec64f5b4454e779364703
Jeremy <jeremy.wu.real@gmail.com>
2022-10-18 15:47:01 +0800

    tweak

--- pages/content/[id].tsx
+++ pages/content/[id].tsx
@@ -1,18 +1,21 @@
-export default function Post ({ postData }) {
+export default function Post({ postData }) {
   return <div>起飞</div>
 }
 
-const paths = [{
-  params: {
-    id: '123',
-    name: 'Jeremy'
+const paths = [
+  {
+    params: {
+      id: '123',
+      name: 'Jeremy',
+    },
   },
-}, {
-  params: {
-    id: '456',
-    name: 'Jeremy 第二篇'
+  {
+    params: {
+      id: '456',
+      name: 'Jeremy 第二篇',
+    },
   },
-}]
+]
 
 export async function getStaticProps({ params }) {
   console.log(params.id)
@@ -20,12 +23,12 @@ export async function getStaticProps({ params }) {
     props: {
       postData: { name: 123 },
     },
-  };
+  }
 }
 
 export async function getStaticPaths() {
   return {
     paths,
     fallback: false,
-  };
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,34 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
-import metadata from '../libs/getMetadata';
+import metadata from '../libs/metadata'
 
-export default function HomePage({ metadata }: {
+export default function HomePage({
+  metadata,
+}: {
   metadata: {
-    name: string,
-    description: string,
+    name: string
+    description: string
   }
 }) {
   return (
     <main>
       <Head>
         <title>{metadata.name}</title>
-        <meta
-          name="description"
-          content={metadata.description}
-        />
+        <meta name="description" content={metadata.description} />
       </Head>
-
 
       <h1 className="title">
         Read <Link href="/posts/123">this page!</Link>
       </h1>
     </main>
-  );
+  )
 }
 
 export async function getStaticProps() {
   return {
     props: {
-      metadata
-    }
+      metadata,
+    },
   }
 }
