aafa1824e190aa5b9780b1602277a5984fc3c7fb
Jeremy <jeremy.wu.real@gmail.com>
2022-10-21 14:39:10 +0800

    tweak

--- pages/[category].tsx
+++ pages/[category].tsx
@@ -14,6 +14,9 @@ export default function Content({ menu, category }: { menu: Menu; category: Cate
       <Head>
         <title>{`${Case.capital(category.type)} - ${process.env.APP_NAME}`}</title>
         <meta name="description" content={metadata.description} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
 
       <Navbar menu={menu} />
