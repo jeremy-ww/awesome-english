1f1a2054c2eae6b95d9c81825bf6f217911bc394
Jeremy <jeremy.wu.real@gmail.com>
2022-11-08 17:31:24 +0800

    build(format): tweak prettier option

--- scripts/add.mjs
+++ scripts/add.mjs
@@ -6,81 +6,88 @@ import addedGlossary from './glossary.json' assert { type: 'json' }
 import addedExpression from './expression.json' assert { type: 'json' }
 import _ from 'lodash'
 import https from 'https'
+import prettierOption from '../.prettierrc.json' assert { type: 'json' }
 
 const glossary = _.keyBy(database.glossary, 'type')
 
 function download(word) {
-  return new Promise((resolve) => {
-    https
-      .get(`https://dict.youdao.com/dictvoice?audio=${word}&type=2`, (res) => {
-        const path = `./public/audios/${word.replace('.', 'dot-')}.mp3`
-        const filePath = fs.createWriteStream(path)
-        res.pipe(filePath)
-        filePath.on('finish', () => {
-          filePath.close()
-          resolve(true)
-        })
-      })
-      .on('error', () => {
-        resolve(false)
-      })
-  })
+	return new Promise((resolve) => {
+		https
+			.get(`https://dict.youdao.com/dictvoice?audio=${word}&type=2`, (res) => {
+				const path = `./public/audios/${word.replace('.', 'dot-')}.mp3`
+				const filePath = fs.createWriteStream(path)
+				res.pipe(filePath)
+				filePath.on('finish', () => {
+					filePath.close()
+					resolve(true)
+				})
+			})
+			.on('error', () => {
+				resolve(false)
+			})
+	})
 }
 
 async function get(type, word) {
-  if (!(type in glossary)) {
-    glossary[type] = {
-      type,
-      content: [],
-    }
-    database.glossary.push(glossary[type])
-  }
-  const existedWord = glossary[type].content.find((v) => v.word === word)
-  if (existedWord) {
-    return
-  }
-  try {
-    const data = await axios
-      .get(
-        `https://dict.youdao.com/jsonapi_s?doctype=json&jsonversion=4&q=${word}&le=en&t=6&client=web&keyfrom=webdict`,
-      )
-      .then((response) => response.data)
-    try {
-      const usPhonetics = `[${data.ec?.word?.usphone ?? 'TBD'}]`
-      const ukPhonetics = `[${data.ec?.word?.ukphone ?? 'TBD'}]`
-      const isSuccess = await download(word)
-      const item = {
-        word,
-        origin: isSuccess ? [`/audios/${word.replace('.', 'dot-')}.mp3`] : [],
-        phonetics: [usPhonetics],
-        reference: '',
-      }
-      if (usPhonetics !== ukPhonetics) {
-        item.phonetics.push(ukPhonetics)
-      }
-      glossary[type].content.unshift(item)
-      glossary[type].content = _.uniqBy(glossary[type].content, 'word')
-      glossary[type].content.sort((a, b) => a.word.localeCompare(b.word))
-      console.log('Finished adding:', word)
-    } catch (e) {
-      console.log(e)
-      console.log('Parse phonetics error', word)
-    }
-  } catch (e) {
-    console.log('Download error', e.message)
-  }
+	if (!(type in glossary)) {
+		glossary[type] = {
+			type,
+			content: [],
+		}
+		database.glossary.push(glossary[type])
+	}
+	const existedWord = glossary[type].content.find((v) => v.word === word)
+	if (existedWord) {
+		return
+	}
+	try {
+		const data = await axios
+			.get(
+				`https://dict.youdao.com/jsonapi_s?doctype=json&jsonversion=4&q=${word}&le=en&t=6&client=web&keyfrom=webdict`,
+			)
+			.then((response) => response.data)
+		try {
+			const usPhonetics = `[${data.ec?.word?.usphone ?? 'TBD'}]`
+			const ukPhonetics = `[${data.ec?.word?.ukphone ?? 'TBD'}]`
+			const isSuccess = await download(word)
+			const item = {
+				word,
+				origin: isSuccess ? [`/audios/${word.replace('.', 'dot-')}.mp3`] : [],
+				phonetics: [usPhonetics],
+				reference: '',
+			}
+			if (usPhonetics !== ukPhonetics) {
+				item.phonetics.push(ukPhonetics)
+			}
+			glossary[type].content.unshift(item)
+			glossary[type].content = _.uniqBy(glossary[type].content, 'word')
+			glossary[type].content.sort((a, b) => a.word.localeCompare(b.word))
+			console.log('Finished adding:', word)
+		} catch (e) {
+			console.log(e)
+			console.log('Parse phonetics error', word)
+		}
+	} catch (e) {
+		console.log('Download error', e.message)
+	}
 }
 
 const promises = []
 
 addedGlossary.forEach((category) => {
-  category?.content.forEach((word) => {
-    promises.push(get(category.type, word))
-  })
+	category?.content.forEach((word) => {
+		promises.push(get(category.type, word))
+	})
 })
 
 database.expression = addedExpression
 
 Promise.all(promises).then(() => {
-  fs.writeFileSync('./libs/database.json', prettier.format(JSON.stringify(database), {}))
+	fs.writeFileSync(
+		'./libs/database.json',
+		prettier.format(JSON.stringify(database), {
+			parser: 'json',
+			...prettierOption,
+		}),
+	)
 })
